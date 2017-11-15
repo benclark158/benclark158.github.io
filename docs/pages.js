735a34301a90aab39060ab967e21b0ed3359d64b
benclark158 <benclark.btc@gmail.com>
2017-11-15 21:48:43 +0000

    worked on pages

--- docs/pages.js
+++ docs/pages.js
@@ -1,4 +1,35 @@
 
-function homePage(){
-	return 'homepage';
+function openPage(page){
+
+    $.ajax({
+        url: "docs/" + page + ".md",
+        type: "POST",
+        data: {},
+        cache: false,
+        success: function (data, status){
+            //create a success messsage
+            $.ajax({
+                url: "https://helloacm.com/api/markdown/",
+                type: "POST",
+                data: {
+                    s: markup
+                },
+                cache: false,
+                success: function (data, status){
+                    //create a success messsage
+                    alert("worked - " + data);
+                },
+                error: function (){
+                    //create an error message
+                    alert("error inner");
+                }
+            });
+        },
+        error: function (){
+            //create an error message
+            alert("error outer");
+        }
+    });
+
+    return "";
 }
