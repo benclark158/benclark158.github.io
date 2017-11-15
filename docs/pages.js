502c1d923eaed5e98807ae77898218e06762e655
benclark158 <benclark.btc@gmail.com>
2017-11-15 22:02:04 +0000

    Created replacements for header and footers

--- docs/pages.js
+++ docs/pages.js
@@ -7,6 +7,11 @@ function openPage(page) {
         cache: false,
         success: function (data, status) {
             //create a success messsage
+
+            data = data.replace('This site is open source. <a href="https://github.com/benclark158/benclark158.github.io/edit/master/docs/home.md">Improve this page</a>.', 'Copyright Ben Clark 2017.');
+
+            data = data.replace('<h1><a href="https://benclark158.github.io/">benclark158.github.io</a></h1>', '');
+
             document.getElementById("htmlSeg").innerHTML = data;
         },
         error: function () {
